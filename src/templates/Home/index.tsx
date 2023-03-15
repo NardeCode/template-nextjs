92c4f984bfc8072ed170d66771bd74300c6e0f85
DieissonNarde <dieissonnarde@gmail.com>
2023-03-14 22:35:42 -0400

    feat: page using chakra ui components

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,13 +1,15 @@
-import { Layout } from '../../components';
+import { Heading, Button, Container, Text } from '@chakra-ui/react';
 
 import * as S from './styles';
 
 export function Home() {
   return (
-    <Layout>
-      <S.HomeContainer>
-        <h1>Home</h1>
-      </S.HomeContainer>
-    </Layout>
+    <Container maxW="90vw" mt={8}>
+      <Heading fontSize={52}>Hello World!</Heading>
+      <Text fontSize="3xl">In love with React & Next</Text>
+      <Button colorScheme="blue" size="lg" fontSize={20} mt={4} px={10} py={8}>
+        Button
+      </Button>
+    </Container>
   );
 }
