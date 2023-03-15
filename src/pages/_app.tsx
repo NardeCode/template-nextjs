87a006568747b3ddf1dd382eee5c3fa0e59327c6
DieissonNarde <dieissonnarde@gmail.com>
2023-03-14 22:32:41 -0400

    style: update theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import type { AppProps } from 'next/app';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '../styles/global';
 import theme from '../styles/theme';
 
+const themeChakra = extendTheme({ theme });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider theme={themeChakra}>
         <Component {...pageProps} />
       </ChakraProvider>
 
