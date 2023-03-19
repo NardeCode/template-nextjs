7a0af2414eb1efd1360240a876ab8bcf99a959e8
DieissonNarde <dieissonnarde@gmail.com>
2023-03-18 20:45:14 -0400

    fix: theme chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,18 @@
 import type { AppProps } from 'next/app';
 
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { ThemeProvider } from 'styled-components';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import GlobalStyle from '../styles/global';
 import theme from '../styles/theme';
 
-const themeChakra = extendTheme({ theme });
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <ChakraProvider theme={themeChakra}>
+    <>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
 
       <GlobalStyle />
-    </ThemeProvider>
+    </>
   );
 }
